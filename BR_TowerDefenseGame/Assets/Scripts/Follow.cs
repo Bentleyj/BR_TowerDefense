673b015b9ecf9c74546ca8e16633e191c60c5160
Bentleyj <jgbentley10@gmail.com>
2017-04-16 14:00:02 +0100

    Initial Commit with Sam's Scene!

---
+++ BR_TowerDefenseGame/Assets/Scripts/Follow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+    public GameObject targetObject;
+ 
+    public float speed = 1.0f;
+
+    private Transform targetPos;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+
+        targetPos = targetObject.transform;
+   
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            transform.position = Vector3.Lerp(transform.position, targetPos.position, speed);
+            
+    }
+
+    }
